3f3815539c954d066d28888807b1abbf8ad95658
Parthiv Prakash <161056456+Pp2k05@users.noreply.github.com>
2026-02-05 16:28:19 +0530

    Update ppok.java

--- ppok.java
+++ ppok.java
@@ -1,26 +1,9 @@
-import java.util.Random;
-
-public class ppok{
+public class ppok {
     
-    public static void main(String[] args) {
-        
-        // Creating the instance of Random class
-        Random r= new Random();
-
-        // Generate random integers in range 0 to 999
-        int r1 = r.nextInt(1000);
-        int r2 = r.nextInt(1000);
-
-        // Printing random integers
-        System.out.println("Random Integers: " + r1);
-        System.out.println("Random Integers: " + r2);
-
-        // Generate random doubles
-        double rd1 = r.nextDouble();
-        double rd2 = r.nextDouble();
-
-        // Printing random doubles
-        System.out.println("Random Doubles: " + rd1);
-        System.out.println("Random Doubles: " + rd2);
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
 }
